36245934be0856f066d87021a4daaef482766eb3
projectaki <akosegypro@gmail.com>
2020-09-06 14:15:26 +0200

    only nearest missing

--- kdtree/KdTree.java
+++ kdtree/KdTree.java
@@ -1,9 +1,13 @@
 import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.RectHV;
-import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdDraw;
 
 public class KdTree {
     private Node root;
+    private Queue<Node> forDraw;
+    private Queue<Point2D> insideRect = new Queue<>();
+    private Point2D champion;
 
     private static class Node {
         private Point2D p;      // the point
@@ -13,15 +17,17 @@ public class KdTree {
         private int size;
         private int depth;
 
-        public Node(Point2D key, int size, int depth) {
+        public Node(Point2D key, int size, int depth, RectHV rect) {
             this.p = key;
             this.size = size;
             this.depth = depth;
+            this.rect = rect;
         }
     }
 
     public KdTree()                               // construct an empty set of points
     {
+        forDraw = new Queue<>();
 
     }
 
@@ -49,22 +55,64 @@ public class KdTree {
     }
 
     private Node insert(Node x, Point2D key) {
+        // x = previous
+        if (x == null) {
+            Node temp = new Node(key, 1, 0, new RectHV(0, 0, 10, 10));
+            forDraw.enqueue(temp);
+            return temp;
+        }
 
-        if (x == null) return new Node(key, 1, 0);
+        double xMin;
+        double yMin;
+        double xMax;
+        double yMax;
 
-        //Odd
         int cmp;
+        // Odd
         if (x.depth % 2 != 0) {
             cmp = Double.compare(key.y(), x.p.y());
+            //Down
+            if (cmp < 0) {
+                xMin = x.rect.xmin();
+                yMin = x.rect.ymin();
+                xMax = x.rect.xmax();
+                yMax = x.p.y();
+            }
+            //UP
+            else {
+                xMin = x.rect.xmin();
+                yMin = x.p.y();
+                xMax = x.rect.xmax();
+                yMax = x.rect.ymax();
+            }
+            // Even
         } else {
             cmp = Double.compare(key.x(), x.p.x());
+            //Left
+            if (cmp < 0) {
+                xMin = x.rect.xmin();
+                yMin = x.rect.ymin();
+                xMax = x.p.x();
+                yMax = x.rect.ymax();
+            }
+            //Right
+            else {
+                xMin = x.p.x();
+                yMin = x.rect.ymin();
+                xMax = x.rect.xmax();
+                yMax = x.rect.ymax();
+            }
         }
         if (cmp < 0) {
             x.lb = insert(x.lb, key);
             x.lb.depth = x.depth + 1;
+            x.lb.rect = new RectHV(xMin, yMin, xMax, yMax);
+            forDraw.enqueue(x.lb);
         } else {
             x.rt = insert(x.rt, key);
             x.rt.depth = x.depth + 1;
+            x.rt.rect = new RectHV(xMin, yMin, xMax, yMax);
+            forDraw.enqueue(x.rt);
         }
         x.size = 1 + size(x.lb) + size(x.rt);
 
@@ -83,9 +131,7 @@ public class KdTree {
         if (p == null) throw new IllegalArgumentException();
         Node x = root;
         while (x != null) {
-            System.out.println("CURRENT:" + x.p);
-            System.out.println("CURENT DPETH:" + x.depth);
-            System.out.println("LEFT CHILD:" + x.lb.p);
+
             //System.out.println("RIGHT CHILD:" + x.rt.p);
             int cmp;
             if (x.depth % 2 != 0) {
@@ -126,44 +172,88 @@ public class KdTree {
 
     public void draw()                         // draw all points to standard draw
     {
+        StdDraw.setPenRadius(0.01);
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.square(0.5, 0.5, 0.5);
+        for (Node n : forDraw) {
+            n.p.draw();
+            n.rect.draw();
 
+        }
     }
 
     public Iterable<Point2D> range(RectHV rect)    // all points that are inside the rectangle (or on the boundary)
     {
         if (rect == null) throw new IllegalArgumentException();
-        Stack<Point2D> insideRect = new Stack<>();
 
+        range(root, rect);
         return insideRect;
+    }
+
+    private void range(Node x, RectHV rect) {
+        if (rect.contains(x.p)) insideRect.enqueue(x.p);
+
+        if (x.lb != null) {
+            if (rect.intersects(x.lb.rect)) range(x.lb, rect);
+        }
+        if (x.rt != null) {
+            if (rect.intersects(x.rt.rect)) range(x.rt, rect);
+        }
     }
 
     public Point2D nearest(Point2D p)             // a nearest neighbor in the set to point p; null if the set is empty
     {
         if (p == null) throw new IllegalArgumentException();
+        nearest(root, p);
+        return champion;
+    }
 
-        return new Point2D(0, 0);
+    private void nearest(Node x, Point2D p) {
+        if (x.depth == 0) {
+            champion = root.p;
+        }
+
+        double recDistX = x.rect.xmin();
+        double recDistY = p.y();
+        if (p.distanceTo(champion) > p.distanceTo(new Point2D(recDistX, recDistY))) {
+            if (x.lb != null) {
+                if (p.distanceTo(x.lb.p) < p.distanceTo(champion)) {
+                    champion = x.lb.p;
+                    nearest(x.lb, p);
+                } else nearest(x.lb, p);
+            }
+            if (x.rt != null) {
+                if (p.distanceTo(x.rt.p) < p.distanceTo(champion)) {
+                    champion = x.rt.p;
+                    nearest(x.rt, p);
+                } else nearest(x.rt, p);
+            }
+        }
     }
 
     public static void main(String[] args)                  // unit testing of the methods (optional)
     {
 
         KdTree kd = new KdTree();
-        Point2D p1 = new Point2D(1, 1);
-        Point2D p2 = new Point2D(0, 1);
-        Point2D p3 = new Point2D(1, 2);
-        Point2D p4 = new Point2D(2, 1);
-        Point2D p5 = new Point2D(2, 2);
-        Point2D p6 = new Point2D(0, 2);
-        Point2D p7 = new Point2D(2, 0);
-        Point2D p8 = new Point2D(3, 5);
+        Point2D p1 = new Point2D(0.2, 0.3);
+        Point2D p2 = new Point2D(0.3, 0.2);
+        Point2D p3 = new Point2D(0.2, 0.1);
+        Point2D p4 = new Point2D(0.4, 0.5);
+        Point2D p5 = new Point2D(0.7, 0.7);
+
         kd.insert(p1);
         kd.insert(p2);
         kd.insert(p3);
         kd.insert(p4);
         kd.insert(p5);
-        kd.insert(p6);
-        kd.insert(p7);
-        kd.insert(p8);
-        System.out.println(kd.root.rt.rt.lb.p);
+
+        kd.draw();
+        RectHV queryRec = new RectHV(0.3, 0.3, 0.7, 0.7);
+        queryRec.draw();
+        Iterable<Point2D> iter = kd.range(queryRec);
+        //System.out.println(iter);
+        kd.nearest(new Point2D(1, 1));
+
+
     }
 }
